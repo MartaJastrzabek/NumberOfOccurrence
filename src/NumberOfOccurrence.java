fb757f578b9aeda54e4d05dd01fb95dc0fa4448d
Marta <marta.jastrzabek@gmail.com>
2019-08-21 19:22:12 +0200

    NumberOfOccurrence improved version

--- src/NumberOfOccurrence.java
+++ src/NumberOfOccurrence.java
@@ -12,32 +12,28 @@ public class NumberOfOccurrence {
         List<Integer> list = new ArrayList<>();
         Set<Integer> set = new TreeSet<>();
 
-        try(
+        try (
                 FileReader fr = new FileReader(file);
                 BufferedReader br = new BufferedReader(fr)
-                ){
+        ) {
             String line = "";
-            while ((line = br.readLine())!= null){
+            while ((line = br.readLine()) != null) {
                 Integer x = Integer.valueOf(line);
                 list.add(x);
                 set.add(x);
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("Program natrafił na nie prawidłowy format w pliku.");
         }
 
-        if(set.isEmpty()){
-            System.out.println("Error!");
-        } else {
-            System.out.println(getOccurrenceNumbers(list, set));
-        }
+        System.out.println(getOccurrenceNumbers(list, set));
     }
 
     private static String getOccurrenceNumbers(List<Integer> list, Set<Integer> set) {
         String occurrence = "";
-        for(Integer i : set){
+        for (Integer i : set) {
             occurrence += i + " - liczba wystąpień: " + Collections.frequency(list, i) + "\n";
         }
         return occurrence;
